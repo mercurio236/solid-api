d6a0dd59576522c43b1182fdc132c3d5c6ba3f9f
Arley Souto <macx15@hotmail.com>
2024-01-18 14:31:50 -0300

    feat: ajustado as configurações do prisma

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import {z} from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
 
-prisma.user.create({
-  data: {
-    name: 'Arley',
-    email: 'arley.souto@hotmail.com',
-  },
+
+app.post('/users', async (req, reply) =>{
+    const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+    })
+
+    const {name, email, password} = registerBodySchema.parse(req.body) // usando o parse ele evita que o código abaixo execute
+
+    await prisma.user.create({
+        data:{
+            name,
+            email,
+            password_hash: password
+        }
+    })
+
+    return reply.status(201).send()
+
 })
