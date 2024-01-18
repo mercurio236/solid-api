04443d8f39ce81b86e67d66bcded37a0d4aa18a8
Arley Souto <macx15@hotmail.com>
2024-01-18 16:46:03 -0300

    feat: interface do repositorio

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { registerUseCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUserRepository } from '@/repositories/prisma/prisma-user-repository'
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,10 +13,12 @@ export async function register(req: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(req.body) // usando o parse ele evita que o código abaixo execute
 
   try {
-    await registerUseCase({
-        name,
-        email,
-        password
+    const prismaUsersRepository = new PrismaUserRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
     })
   } catch (error) {
     return reply.status(409).send()
