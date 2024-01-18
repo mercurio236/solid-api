9eac8d688b209851aab39a678e364b759a7e36a2
Arley Souto <macx15@hotmail.com>
2024-01-18 14:55:32 -0300

    feat: separados os arquivos

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,8 @@
 import fastify from 'fastify'
-import {z} from 'zod'
-import { prisma } from './lib/prisma'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
+app.register(appRoutes)
 
 
-app.post('/users', async (req, reply) =>{
-    const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6)
-    })
-
-    const {name, email, password} = registerBodySchema.parse(req.body) // usando o parse ele evita que o código abaixo execute
-
-    await prisma.user.create({
-        data:{
-            name,
-            email,
-            password_hash: password
-        }
-    })
-
-    return reply.status(201).send()
-
-})
