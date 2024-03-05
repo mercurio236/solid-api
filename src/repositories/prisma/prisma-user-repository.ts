5ceda94fa0093aa2f3be62c0905211e840de512a
Arley Souto <macx15@hotmail.com>
2024-03-05 17:16:07 -0300

    feat: validando repositorio do prisma

--- src/repositories/prisma/prisma-user-repository.ts
+++ src/repositories/prisma/prisma-user-repository.ts
@@ -3,9 +3,16 @@ import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../use-repository'
 
 export class PrismaUserRepository implements UsersRepository {
-  findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
   }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
@@ -15,6 +22,7 @@ export class PrismaUserRepository implements UsersRepository {
 
     return user
   }
+  
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
