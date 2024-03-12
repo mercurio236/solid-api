75c02f4ad7464cf9d26a0f355edbbf9c180df54c
Arley Souto <macx15@hotmail.com>
2024-03-12 20:08:35 -0300

    feat: ajustando refresh token adicionando no cookie

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -12,30 +12,48 @@ export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
   const { email, password } = authenticateBodySchema.parse(req.body)
 
   try {
-   const authenticateUseCase = makeAuthenticateUseCase()
+    const authenticateUseCase = makeAuthenticateUseCase()
 
-    const {user} = await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
     })
 
-    const token = await reply.jwtSign({}, {
-      sign:{
-        sub: user.id
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
       }
-    })
+    )
 
-    return reply.status(200).send({
-      token
-    })
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      }
+    )
 
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/', // aqui diz que toda aplicação vai ser acesso ao refresh
+        secure: true, // aqui faz com que nosso seja encripitado atraves do https
+        sameSite: true, //vai ser acessivel apenas dentro do mesmo dominio
+        httpOnly: true, // vai ter acesso apenas aqui no back não vai ter no front
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (error) {
-    if(error instanceof InvalidCredentialsError){
-      return reply.status(400).send({message: error.message})
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message })
     }
 
     throw error
   }
-
-  
 }
